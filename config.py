04a99b2ebea242e8be820e7e51811035bc3adecb
root <root@v1479855.hosted-by-vdsina.ru>
2023-01-21 15:58:08 +0000

    test commits

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12QWaszx34//@localhost/main_db_v1'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12qwaszx34//@localhost/AppleDB_v2'
+SQLALCHEMY_DATABASE_URI = 'postgresql://egorvasilev:12qwaszx34@localhost/appledb_v2'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://egorvasilev:12QWaszx34//@localhost/appledb_v2'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 # def get_env_variable(name):
 #     try:
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #!flask/bin/python
 from app import app # Importing app Flask-variable from our packet app 
 
-app.run(debug = True)
+app.run(debug = True, host='0.0.0.0')
